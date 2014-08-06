ef30887857558ed8aa41a0b005a144c1ccfec307
dribretrop <see.teerasak@gmail.com>
2014-08-06 17:37:46 +0700

    re-arrange

--- 13_requiredfield.php
+++ 13_requiredfield.php
@@ -1,64 +1,37 @@
 <html>
 <head>
-<title>11_Requiedfield</title>
+<style>
+    #error {color: #f00 ;}
+</style>
+    <title>13_requiredfield</title>
 </head>
-    
     <body>
-        <?php
-           // echo "test<br>";
-            
-            //valiable error and value
-            $errName = $errEmail = $errWebsite = $errGender ="";
-            $name = $email = $website = $gender = "";
-            //echo "test<br>";
-            
-            if($_SERVER["REQUEST_METHOD"]=="POST") {
-                if(empty($_POST["name"])) {
-                    $errName = "Invalid Name";
-                } else {
-                    $name = $_POST["name"];
-                } 
-
-                if(empty($_POST["email"])) {
-                    $errEmail = "Invalid Email";
-                } else {
-                    $email = $_POST["email"];
-                }
-                if(empty($_POST["website"])) {
-                    $errWebsite = "Invalid Website";
-                } else {
-                    $website = $_POST["website"];
-                }
-                if(empty($_POST["gender"])) {
-                    $errGender = "Please select gender";
-                } else {
-                    $gender = $_POST["gender"];
-                }
-            }
-               
-        ?>
-        <h1>Please input data</h1>
-
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-            Name : <input type="text" name="name"> <?php echo $errName; ?>
-            <br><br>
-            eMail : <input type="text" name="email"> <?php echo $errEmail; ?>
-            <br><br>
-            Website : <input type="text" name="website"> <?php echo $errWebsite; ?>
-            <br><br>
-            Gender : <input type="radio" name="gender" value="Male">Male
-                     <input type="radio" name="gender" value="Female">Female <?php echo $errGender; ?><br><br>
-            <input type="submit" name="submit" value="Submit">
+     <h1>Input your data :</h1>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["SELF_PHP"]); ?>>">
+    Name : <input type="text" name="name"> <span id="error"> * require </span><br>
+    Email : <input type="text" name="email"><span id="error"> * require</span><br>
+    Comment : <textarea rows="5" cols="20" name="comment"></textarea><br>
+    Gender : <input type="radio" name="gender" value="Male"> Male 
+               <input type="radio" name="gender" value="Female"> Female <span id="error">* Please select gender</span><br>
+    <input type="submit" name="submit" value="Submit">
         </form>
-       
+        
         <?php
-           echo "$name <br>";
-           echo "$email <br>";
-           echo "$website <br>";
-           echo "$gender <br>";
-       ?>
-    </body>      
+                    
+        ?>
+
+        <?php
+            function validate($data){
+                $data = trim($data);
+                $data = str_replace("\\","",$data);
+                $data = str_replace("/","",$data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+        ?>
+
+        
 
 
-
+    </body>
 </html>
