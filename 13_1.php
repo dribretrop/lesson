2ab50ec4383c95c29598b90deef8e4d90c7bbb4b
dribretrop <see.teerasak@gmail.com>
2014-08-07 15:46:15 +0700

    test

--- 13_1.php
+++ 13_1.php
@@ -33,25 +33,38 @@
            } else {
                $gender = $_POST["gender"];
            }
+
     } 
-    
+ 
+           
+           
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
+        <style>
+            #error {color: #f00 ;}
+        </style>
         <title>13_1 Required Field</title>
     </head>
     <body>
         <h1>Require Data</h1>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Name : <input type="text" name="name"> * <br>
-        eMail : <input type="text" name="email"> * <br>
+        Name : <input type="text" name="name"> <span id="error">* <?php echo $errName; ?></span><br>
+        eMail : <input type="text" name="email"><span id="error"> * <?php echo $errEmail; ?></span><br>
         Comment : <textarea name="comment"></textarea> <br>
         Gender : <input type="radio" name="gender" value="Male"> Male
-                <input type="radio" name="gender" value="Female"> Female * <br><br> 
+                <input type="radio" name="gender" value="Female"> Female <span id="error">* <?php echo $errGender; ?>  </span><br><br> 
         <input type="submit" name="submit" value="Submit">
         </form>
+      
+        <?php
+           echo "$name <br>";
+           echo "$email <br>";
+           echo "$comment <br>";
+           echo "$gender";
+        ?>
     </body>
 </html>
