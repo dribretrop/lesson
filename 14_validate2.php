6ef66062ef25f86610ae893487616025afba4fe0
dribretrop <see.teerasak@gmail.com>
2014-08-13 15:11:13 +0700

    14_validate2.php

--- 14_validate2.php
+++ 14_validate2.php
@@ -18,11 +18,41 @@
         } else {
             $name = validate($_POST["name"]);
             // check letter and whitespace
-            if (!preg_match("/^[a-zA-z ]*$/",$name))
+            if (!preg_match("/^[a-zA-z .]*$/",$name)){
             $errName = "Name required only letter and white space";
+            }
+        }
+    
+        if (empty($_POST["email"])){
+            $errEmail = "Email required";
+        } else {
+            $email = validate($_POST["email"]);
+            if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)){
+                $errEmail = "Invalid email format";                
+            }
+        }
+
+        if (empty($_POST["website"])){
+            $errWebsite = "";
+        } else {
+            $website = validate($_POST["website"]);
+            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)){
+                $errWebsite = "* Invalid site format";
+            }
+        }
+
+        if (empty($_POST["comment"])){
+            $errComment = "";
+        } else {
+            $comment = $_POST["comment"];
+        }
+
+        if (empty($_POST["gender"])){
+            $errGender = "Plz select gender";
+        } else {
+            $gender = $_POST["gender"];
         }
     }
-
 ?>
 
 <!DOCTYPE html>
@@ -37,12 +67,21 @@
     <body>
         <h1>Input data</h1>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["SELF_PHP"]); ?>">
-        Name : <input type="text" name="name"><span id="error">* </span><br>
-        Email : <input type="text" name="email"><span id="error">* </span><br>
-        Website : <input type="text" name="website"><br>
+        Name : <input type="text" name="name"><span id="error">* <?php echo $errName ; ?></span><br>
+        Email : <input type="text" name="email"><span id="error">* <?php echo $errEmail ; ?></span><br>
+        Website : <input type="text" name="website"><span id="error"> <?php echo $errWebsite ; ?></span><br>
         Comment : <textarea cols="30" rows="5" name="comment">You can comment here !</textarea><br>
-        Gender : <input type="radio" name="gender" value="male">Male <input type="radio" name="gender" value="female">Female <span id="error">*</span><br>
+        Gender : <input type="radio" name="gender" value="Male">Male <input type="radio" name="gender" value="Female">Female <span id="error">* <?php echo $errGender ; ?></span><br>
         <input type="submit" name="submit" value="Submit">
         </form>
+
+        <?php
+            echo "$name <br>";
+            echo "$email <br>";
+            echo "$website <br>";
+            echo "$comment <br>";
+            echo "$gender <br>";
+        ?>
     </body>
+    
 </html>
